58deae6a2bc5011f6b32f8ef96bcce53d54476d1
Brahim Sioini <besioini@gmail.com>
2024-02-07 16:24:19 -0500

    Added get, update profile and delete account functionality

--- scripts/auth.js
+++ scripts/auth.js
@@ -35,6 +35,25 @@ document.addEventListener('DOMContentLoaded', () => {
             logout();
         });
     }
+
+    if (document.getElementById('profile-container')) {
+        getProfile();
+    }
+
+    if (updateButton) {
+        updateButton.addEventListener('click', function(event){
+            event.preventDefault();
+            updateProfile();
+        });
+    }
+    
+    if (deleteButton) {
+        deleteButton.addEventListener('click', () => {
+            if (confirm('Are you sure you want to delete your account? This action cannot be undone.')) {
+                deleteAccount();
+            }
+        });
+    }
 });
 
 const register = async() => {
@@ -102,3 +121,108 @@ const logout = () => {
     window.location.href = '../../pages/login.html';
 }
 
+const getProfile = async () => {
+    try {
+        const token = localStorage.getItem('authToken');
+        const userId = getUserId(token);
+        const response = await fetch(`http://localhost:5000/api/users/getProfile/${userId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        const data = await response.json();
+        if (response.ok) {
+            displayProfile(data);
+        } else {
+            console.error('Failed to fetch profile:', data.message);
+        }
+    } catch (error) {
+        console.error('Error fetching profile:', error);
+    }
+};
+
+const displayProfile = (userData) => {
+    const profileContainer = document.getElementById('profile-container');
+    profileContainer.innerHTML = `
+        <h2>User Profile</h2>
+        <p><strong>First Name:</strong> ${userData.firstname}</p>
+        <p><strong>Last Name:</strong> ${userData.lastname}</p>
+        <p><strong>Username:</strong> ${userData.username}</p>
+        <p><strong>Email:</strong> ${userData.email}</p>
+    `;
+};
+
+const updateProfile = async () => {
+    let updatedFields = {};
+    const firstname = document.getElementById('update-firstname').value;
+    if (firstname) updatedFields.firstname = firstname;
+
+    const lastname = document.getElementById('update-lastname').value;
+    if (lastname) updatedFields.lastname = lastname;
+
+    const username = document.getElementById('update-username').value;
+    if (username) updatedFields.username = username;
+
+    const email = document.getElementById('update-email').value;
+    if (email) updatedFields.email = email;
+
+    if (Object.keys(updatedFields).length === 0) {
+        console.log('No changes detected. Please update at least one field.');
+        alert('No changes detected. Please update at least one field.')
+        return;
+    }
+
+    const token = localStorage.getItem('authToken');
+
+    const requestOptions = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(updatedFields)
+    };
+
+    try {
+        const token = localStorage.getItem('authToken');
+        const userId = getUserId(token);        
+        const response = await fetch(`http://localhost:5000/api/users/updateProfile/${userId}`, requestOptions);
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log('Profile updated successfully:', data);
+            document.getElementById('updateForm').style.display = 'none';
+            window.location.reload();
+        } else {
+            console.error('Failed to update profile:', data.message);
+            alert('Failed to update profile');
+        }
+    } catch (error) {
+        console.error('Error updating profile:', error);
+        alert('Error updating profile');
+    }
+};
+
+const deleteAccount = async () => {
+    try {
+        const token = localStorage.getItem('authToken');
+        const userId = getUserId(token);
+        const response = await fetch(`http://localhost:5000/api/users/deleteAccount/${userId}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        const data = await response.json();
+        if (response.ok) {
+            console.log('Account deleted successfully');
+            logout();
+        } else {
+            console.error('Failed to delete account:', data.message);
+        }
+    } catch (error) {
+        console.error('Error deleting account:', error);
+        alert('Error deleting account');
+    }
+};
